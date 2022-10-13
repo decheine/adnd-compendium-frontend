e953dab1db1e5742578fece1d0345976a1495124
decheine <decheine@wisc.edu>
2022-10-12 20:24:17 -0500

    Show Footer on every page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router";
 import BreadCrumb from "./BreadCrumb/BreadCrumbFrame";
+import Footer from "./Footer";
 
 import './Layout.css'
 
@@ -9,10 +10,13 @@ const Layout = ({ children } :any) => {
     const breadCrumbs = pathname.split("/");
     // console.log("pathname: " + pathname, breadCrumbs)
     return (
+      <>
       <div className="layout">
         {pathname !== "/" ? <BreadCrumb title={breadCrumbs[1]} setting={breadCrumbs[2]} book={breadCrumbs[3]} monster_key={breadCrumbs[4]}/> : null}
         <div className="layout-main">{children}</div>
       </div>
+      <Footer/>
+      </>
     );
 };
 
